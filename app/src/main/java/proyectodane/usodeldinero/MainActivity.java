4cc1928415c74ac39dc77b0944325737822f1ba5
mart <martineq@gmail.com>
2018-04-21 19:57:45 -0300

    Splash Screen

--- app/src/main/java/proyectodane/usodeldinero/MainActivity.java
+++ app/src/main/java/proyectodane/usodeldinero/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme); // Splash Screen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
