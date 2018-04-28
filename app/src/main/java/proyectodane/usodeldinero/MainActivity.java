d223effae3b743bf54088ca68af4595f7824a24c
mart <martineq@gmail.com>
2018-04-28 15:12:05 -0300

    Ubicación de componentes en pantalla

--- app/src/main/java/proyectodane/usodeldinero/MainActivity.java
+++ app/src/main/java/proyectodane/usodeldinero/MainActivity.java
@@ -4,11 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Prueba al presionar un boton */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    /** Envía a la pantalla de compra al presionar el botón */
+    public void sendToPurchase(View view) {
+       // Intent intent = new Intent(this, PurchaseActivity.class);
+       // startActivity(intent);
     }
 
 
