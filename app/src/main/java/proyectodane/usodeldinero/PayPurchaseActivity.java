c0c01b2ff1b2e7fe46242ce74f91b23b5a9d0a34
mart <martineq@gmail.com>
2018-04-30 14:35:10 -0300

    Agrego e inicio la activity PayPurchase

--- app/src/main/java/proyectodane/usodeldinero/PayPurchaseActivity.java
+++ app/src/main/java/proyectodane/usodeldinero/PayPurchaseActivity.java
@@ -1,5 +1,6 @@
 package proyectodane.usodeldinero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class PayPurchaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_purchase);
+
+        // Obtengo el intent que inició el activity y extraigo el valor del total
+        Intent intent = getIntent();
+        String st_total = intent.getStringExtra(getString(R.string.tag_total_value));
+        
     }
 }
