0379eed931841a2220916f88322cd5e4e693f563
mart <martineq@gmail.com>
2018-04-30 16:03:06 -0300

    Acción en botones y formato

--- app/src/main/java/proyectodane/usodeldinero/PayPurchaseActivity.java
+++ app/src/main/java/proyectodane/usodeldinero/PayPurchaseActivity.java
@@ -3,8 +3,11 @@ package proyectodane.usodeldinero;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PayPurchaseActivity extends AppCompatActivity {
+
+    String st_total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +16,24 @@ public class PayPurchaseActivity extends AppCompatActivity {
 
         // Obtengo el intent que inició el activity y extraigo el valor del total
         Intent intent = getIntent();
-        String st_total = intent.getStringExtra(getString(R.string.tag_total_value));
+        st_total = intent.getStringExtra(getString(R.string.tag_total_value));
 
     }
+
+    /** Envía a la pantalla principal */
+    public void sendToMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    /** Envía a la pantalla de control de vuelto */
+    public void sendToControlChange(View view) {
+        this.sendToMain(view); //TODO: borrar esta línea y descomentar las líneas del final al crear "ControlChangeActivity"
+
+        /*Intent intent = new Intent(this, ControlChangeActivity.class);
+        intent.putExtra(getString(R.string.tag_total_value),st_total);
+        startActivity(intent);*/
+    }
+
+
 }
