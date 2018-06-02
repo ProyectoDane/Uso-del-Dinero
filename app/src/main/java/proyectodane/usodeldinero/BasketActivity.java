69146889cfa825bcc584eb9bbc19fc527dfc99c0
mart <martineq@gmail.com>
2018-06-02 19:23:22 -0300

    Agrego biblioteca Design para uso de mensajes Snackbar. Agrego mensajes emergentes para informar costo exedido en la compra.

--- app/src/main/java/proyectodane/usodeldinero/BasketActivity.java
+++ app/src/main/java/proyectodane/usodeldinero/BasketActivity.java
@@ -1,6 +1,7 @@
 package proyectodane.usodeldinero;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class BasketActivity extends AppCompatActivity {
+
+    /** Valor total guardado en la billetera
+     * */
+    Double d_walletTotal;
 
     /** Valor total de la compra
      * */
@@ -24,6 +29,7 @@ public class BasketActivity extends AppCompatActivity {
         setContentView(R.layout.activity_basket);
         d_total = 0.0;
         et_productValue = (EditText) findViewById(R.id.editText); // Obtiene el ID del EditText del valor ingresado
+        d_walletTotal = 20.0; // TODO: Cargar el valor real de la billetera
     }
 
 
@@ -40,32 +46,40 @@ public class BasketActivity extends AppCompatActivity {
     public void addToTotal(View view) {
 
         String st_total;
-        Double d_value;
+        Double d_newValue;
 
         // Obtiene el valor ingresado, comprobando el formato válido
         try {
-            d_value = Double.valueOf(et_productValue.getText().toString());
+            d_newValue = Double.valueOf(et_productValue.getText().toString());
         } catch (NumberFormatException e) {
-            d_value = 0.0;
+            d_newValue = 0.0;
         }
 
         // TODO: Ver el redondeo o truncamiento que se hace al ingresar importes, y si limito la cantidad de dígitos enteros
         // TODO: Ideal -> Solo permitir hasta 2 decimales, y una cantidad máxima de N enteros.
 
         // Suma el valor al total anterior, redondeando para obtener hasta 2 decimales
-        d_value = Math.floor(d_value * 100) / 100;
+        d_newValue = Math.floor(d_newValue * 100) / 100;
+        Double d_newTotal = d_total + d_newValue;
+        d_newTotal = Math.floor(d_newTotal * 100) / 100;
 
         // TODO: Calcular si el total en la billetera alcanza para pagar la compra
+        // Si el total en la billetera no alcanza para pagar la compra total,
+        // descarto el producto que se está por agregar y aviso que el dinero es insuficiente.
+        // Caso contrario, agrego el importe al total de la compra
+        if (d_newTotal>d_walletTotal){
+            Snackbar.make(findViewById(R.id.myCoordinatorLayout),R.string.insufficient_funds,Snackbar.LENGTH_LONG).show();
+        } else {
+            d_total = d_newTotal;
 
-        d_total = d_total + d_value;
-        d_total = Math.floor(d_total * 100) / 100;
+            // Convierte el valor a string
+            st_total = getString(R.string.total_value) + String.valueOf(d_total); // Agrego el texto completo
 
-        // Convierte el valor a string
-        st_total = getString(R.string.total_value) + String.valueOf(d_total); // Agrego el texto completo
+            // Actualiza el valor total actual en la vista
+            TextView textView = findViewById(R.id.textView);
+            textView.setText(st_total);
 
-        // Actualiza el valor total actual en la vista
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(st_total);
+        }
 
         // Blanquea el valor a ingresar en la vista
         et_productValue.setText(getString(R.string.empty_string));
