24549de4a87b29999cf3be888969d6e82c82d596
mart <martineq@gmail.com>
2019-03-03 18:15:54 -0300

    Refactor: Activity Wallet pasa a Fragment. Refactor para uso de Tabs. Correcciones menores.

--- app/src/main/java/proyectodane/usodeldinero/WalletFragment.java
+++ app/src/main/java/proyectodane/usodeldinero/WalletFragment.java
@@ -1,16 +1,18 @@
 package proyectodane.usodeldinero;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.util.ArrayList;
 
-public class WalletActivity extends AppCompatActivity {
+public class WalletFragment extends Fragment {
 
     /**
      * Importe total a de la billetera, con la suma de la carga
@@ -37,43 +39,79 @@ public class WalletActivity extends AppCompatActivity {
      */
     private SnackBarManager sb;
 
+    /**
+     * Vista instanciada
+     */
+    View rootView;
+
+
+    // Todo: Ver si sirve este ejemplo par paso de parámetros. Si no se usa, borrarlo
+    //        public WalletFragment() { }
+    //
+    //        /**
+    //         * Devuelve una nueva instancia del Fragment, dado un número de sección
+    //         */
+    //        public static WalletFragment newInstance(int sectionNumber) {
+    //            WalletFragment fragment = new WalletFragment();
+    //            Bundle args = new Bundle();
+    //            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+    //            fragment.setArguments(args);
+    //            return fragment;
+    //        }
+
+    // Llamado en la parte inicial de la creación del Fragment.
+    // Se inicializan objetos no gráficos.
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wallet);
+    }
+
+
+    // Llamado para usar el inflate con el layout del Fragment.
+    // Se inicializa objetos gráficos
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_wallet, container, false);
+        return rootView;
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
         // Inicio la lista de valores a cargar en la billetera
         newLoadMoneyValueNames = new ArrayList<String>();
 
         // Actualizo el valor del total, inicio el subtotal en cero y muestro en pantalla
         st_subtotal = getString(R.string.value_0);
-        st_total = WalletManager.getInstance().obtainTotalCreditInWallet(this);
+        st_total = WalletManager.getInstance().obtainTotalCreditInWallet(getActivity());
         refreshSubtotalAndTotal();
 
         // Inicio el SnackBarManager para luego crear mensajes emergentes
         sb = new SnackBarManager();
 
         // Obtengo todos los valores a mostrar para la carga de la billetera
-        ArrayList<String> moneyValueNames = WalletManager.getInstance().obtainMoneyValueNamesOfValidCurrency(this);
+        ArrayList<String> moneyValueNames = WalletManager.getInstance().obtainMoneyValueNamesOfValidCurrency(getActivity());
 
         // Cargo el slide de imágenes y puntos indicadores
         // Parámetros:  + (1)Contexto
         //              + (2)ViewPager con su (3)FragmentManager y sus (4)moneyValueNames (nombres de las imágenes)
         //              + (5)LinearLayout y sus (6)(7)imágenes representando al punto
-        imageSlideManager = new ImageSlideManager(this,
-                (ViewPager) findViewById(R.id.pager_wallet),
-                getSupportFragmentManager(),
+        imageSlideManager = new ImageSlideManager(getActivity(),
+                (ViewPager) rootView.findViewById(R.id.pager_wallet),
+                getActivity().getSupportFragmentManager(),
                 moneyValueNames,
-                (LinearLayout) findViewById(R.id.SliderDots_wallet),
-                ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot),
-                ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
+                (LinearLayout) rootView.findViewById(R.id.SliderDots_wallet),
+                ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot),
+                ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.nonactive_dot));
 
     }
 
-    @Override
-    public void onBackPressed() {
-        imageSlideManager.defaultOnBackPressed();
-    }
+
+
+// TODO: Verificar si quedan o se sacan las funciones...
+// TODO: y en caso de usarse deben pasarse dentro del onClick()
 
     /**
      * Cancela la carga en la billetera
@@ -82,39 +120,43 @@ public class WalletActivity extends AppCompatActivity {
         sendToMain(view);
     }
 
+
     /**
      * Envía a la pantalla principal
      * */
     public void sendToMain(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(getActivity(), MainActivity.class);
         startActivity(intent);
     }
+
 
     /**
      * Muestra el texto de ayuda para este activity
      **/
     public void showHelp(View view) {
-        sb.showTextIndefiniteOnClickActionDisabled(findViewById(R.id.coordinatorLayout_Wallet),getString(R.string.help_text_wallet),10);
+        sb.showTextIndefiniteOnClickActionDisabled(rootView.findViewById(R.id.coordinatorLayout_Wallet),getString(R.string.help_text_wallet),10);
     }
+
 
     /**
      * Actualiza el valor de la carga y del total
      **/
     public void refreshSubtotalAndTotal() {
-        TextView textView = findViewById(R.id.textView6);
+        TextView textView = rootView.findViewById(R.id.textView6);
         textView.setText(getString(R.string.load_cash_sign) + st_subtotal + " - " + getString(R.string.total_cash_sign) + st_total);
     }
+
 
     /**
      * Sumo el valor seleccionado al subtotal de carga en billetera
      **/
-    public void addValueToSubtotal (View view) {
+    public void addValueToSubtotalTAB (View view) {
 
         // Obtengo el ID del valor elegido
         String st_valueID = imageSlideManager.getActualValueID();
 
         // Obtengo el valor monetario a partir del ID
-        String st_value = WalletManager.getInstance().obtainValueFormID(this,st_valueID);
+        String st_value = WalletManager.getInstance().obtainValueFormID(getActivity(),st_valueID);
 
         // Agrego el ID a la lista para la futura carga, sumo el valor al subtotal y total para luego mostrarlo
         newLoadMoneyValueNames.add(st_valueID);
@@ -124,8 +166,9 @@ public class WalletActivity extends AppCompatActivity {
 
         // Creo el mensaje para notificar el valor seleccionado a sumar a la billetera y lo muestro
         String st_snackBarText = getString(R.string.value_selected_for_load) + st_value;
-        sb.showTextShortOnClickActionDisabled(findViewById(R.id.coordinatorLayout_Wallet),st_snackBarText,2);
+        sb.showTextShortOnClickActionDisabled(rootView.findViewById(R.id.coordinatorLayout_Wallet),st_snackBarText,2);
     }
+
 
     /**
      * Agrega la carga de dinero seleccionada en la billetera y luego envía a la pantalla principal
@@ -134,18 +177,20 @@ public class WalletActivity extends AppCompatActivity {
 
         // Guardo todos los valores seleccionados
         for(String currentNewLoadMoneyValueName : newLoadMoneyValueNames) {
-            WalletManager.getInstance().addCurrencyInWallet(this,currentNewLoadMoneyValueName);
+            WalletManager.getInstance().addCurrencyInWallet(getActivity(),currentNewLoadMoneyValueName);
         }
 
         sendToMain(view);
     }
 
+
     /**
      * Envía a la pantalla de configuración
      * */
     public void sendToConfiguration(View view) {
-        Intent intent = new Intent(this, ConfigurationActivity.class);
+        Intent intent = new Intent(getActivity(), ConfigurationActivity.class);
         startActivity(intent);
     }
 
 }
+
